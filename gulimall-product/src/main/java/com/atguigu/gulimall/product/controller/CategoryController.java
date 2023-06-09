e3af2009c08499849cf711e6724e5941b180243a
HUST-996 <1278921534@qq.com>
2023-06-09 16:37:32 +0800

    OSS上传文件

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
@@ -74,11 +74,14 @@ public class CategoryController {
 
     /**
      * 删除
+     * @RequestBody：获取请求体
      */
     @RequestMapping("/delete")
     public R delete(@RequestBody Long[] catIds){
-		categoryService.removeByIds(Arrays.asList(catIds));
 
+        //1.检查当前删除的菜单，是否被别的地方引用
+//		categoryService.removeByIds(Arrays.asList(catIds));
+        categoryService.removeMenuByIds(Arrays.asList(catIds));
         return R.ok();
     }
 
--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,5 +1,7 @@
 package com.atguigu.gulimall.product;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 
@@ -10,17 +12,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GulimallProductApplicationTests {
 
-
     @Autowired
     BrandService brandService;
+
+    @Autowired
+    OSS ossClient;
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws FileNotFoundException {
 
 //        BrandEntity brandEntity = new BrandEntity();
 //        brandEntity.setDescript("");
@@ -28,8 +35,12 @@ public class GulimallProductApplicationTests {
 //        System.out.println(brandService);
 //        brandService.save(brandEntity);
 //        System.out.println("保存成功");
-        List<BrandEntity> brand_id = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 9L));
-        brand_id.forEach(System.out::println);
+//        List<BrandEntity> brand_id = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 9L));
+//        brand_id.forEach(System.out::println);
+        InputStream inputStream = new FileInputStream("D:\\backgroundImage\\wanye.jpg");
+        ossClient.putObject("gulimall-yuming", "wanye.jpg", inputStream);
+        ossClient.shutdown();
+        System.out.println("上传完成...");
     }
 
 }
